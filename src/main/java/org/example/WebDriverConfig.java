80c9a16ba1ba14438412ed1b03d638527b8de57a
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-04 16:26:02 -0300

    implementando método para configurar webDriver

--- src/main/java/org/example/WebDriverConfig.java
+++ src/main/java/org/example/WebDriverConfig.java
@@ -10,9 +10,26 @@ public class WebDriverConfig {
     private static WebDriver driver;
     public static WebDriver getDriver(String browser){
         if (driver == null){
-            //initializeWebDriver(browser);
+            initializeWebDriver(browser);
         }
         return driver;
     }
-
+    public static void initializeWebDriver(String browser){
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid browser name: " + browser);
+        }
+    }
 }
