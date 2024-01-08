09291ac60234a85145b1a078b3ace88af0986607
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-08 16:13:40 -0300

    Iniciando primeira atividade

---
+++ src/main/java/org/example/ex1.java
@@ -0,0 +1,10 @@
+package org.example;
+/*Primeira atividade: inicializando teste de login no site sauce.demo
+ Utilizando xpath como localizador
+ enviando chaves de acesso(email e senha)*/
+
+public class ex1 {
+    public static void main(String[] args) {
+
+    }
+}
