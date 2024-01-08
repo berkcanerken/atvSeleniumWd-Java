5979d41de7ee1c53e9ca0a6616fcfc037fb44edf
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-08 16:42:05 -0300

    escrevendo instruções

--- src/main/java/org/example/ex1.java
+++ src/main/java/org/example/ex1.java
@@ -9,7 +9,10 @@ import org.openqa.selenium.WebDriver;
 public class ex1 {
 
     public static void main(String[] args) {
-        //caso você utilize o drive de outro navegador apenas digite o nome dele no parametro da função abaixo
+        /*caso você utilize o drive de outro navegador apenas digite
+        o nome dele no parametro da função abaixo
+        */
+        //Chrome, edge, firefox
         WebDriver driver = WebDriverConfig.getDriver("chrome");
 
         driver.manage().window().maximize();
