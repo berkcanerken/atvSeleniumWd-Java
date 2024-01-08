41180fe8e2192c082bdf9f85ab34ebf36cecd59b
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-08 16:19:20 -0300

    implementando teste de login

--- src/main/java/org/example/ex1.java
+++ src/main/java/org/example/ex1.java
@@ -3,8 +3,20 @@ package org.example;
  Utilizando xpath como localizador
  enviando chaves de acesso(email e senha)*/
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ex1 {
+
     public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver("chrome");
+
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/input")).click();
 
     }
 }
