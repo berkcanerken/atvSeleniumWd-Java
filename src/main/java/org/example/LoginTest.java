adce830f6265f29952378f93e9ee91bbae13444c
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-09 12:04:39 -0300

    otimizando método de abrir Url

--- src/main/java/org/example/LoginTest.java
+++ src/main/java/org/example/LoginTest.java
@@ -7,14 +7,15 @@ import org.openqa.selenium.WebDriver;
 public class LoginTest {
     private WebDriver driver;
 
+
     public LoginTest(WebDriver driver) {
         this.driver = driver;
     }
 
     public void login(String username, String password) {
 
-        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("user-name")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("loginButton")).click();
+        driver.findElement(By.xpath("//input[contains(@id, 'login-button')]")).click();
     }
 }
--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -3,12 +3,14 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 
 public class MainTest {
-    public static String urlBase;
+    public static String urlBase = "https://www.saucedemo.com";
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver("chrome");
         WebDriverConfig config = new WebDriverConfig(urlBase);
         LoginTest testLogin = new LoginTest(driver);
 
+        //Abrindo Site
+        config.openUrl();
         //Teste de login
         testLogin.login("standard_user", "secret_sauce");
     }
