32275bd96657cd8b4f26272e56bc1f218cff5267
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2024-01-09 11:48:00 -0300

    adicionando novos métodos

--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -3,8 +3,10 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 
 public class MainTest {
+    public static String urlBase;
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver("chrome");
+        WebDriverConfig config = new WebDriverConfig(urlBase);
         LoginTest testLogin = new LoginTest(driver);
 
         //Teste de login
--- src/main/java/org/example/WebDriverConfig.java
+++ src/main/java/org/example/WebDriverConfig.java
@@ -8,11 +8,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverConfig {
     public static WebDriver driver;
+    private String url;
+
+    public WebDriverConfig(String url) {
+        this.url = url;
+    }
+
     public static WebDriver getDriver(String browser){
         if (driver == null){
             initializeWebDriver(browser);
         }
         return driver;
+    }
+    public  void openUrl(){
+        driver.manage().window().maximize();
+        driver.get(url);
     }
     public static void initializeWebDriver(String browser){
         switch (browser.toLowerCase()) {
